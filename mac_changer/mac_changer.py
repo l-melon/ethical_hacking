064a5ce1f6620763e98e6a10a552921244dd4aee
lumin#kali <baolong1027@icloud.com>
2022-11-20 00:01:00 -0500

    some change

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+interface = input("interface > ")
+new_mac = input("new MAC > ")
+
+print(f"[+] Changing MAC address for {interface} to {new_mac}")
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
